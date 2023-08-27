961c7ad018c40c361d56e39358c1847ca409dd40
Lilly Gullett <141647933+Lilly-Gullett@users.noreply.github.com>
2023-08-27 14:05:44 -0600

    Add function for computer selection to javascript
    This commit adds the function needed for the computer to select randomly
    between rock, paper and scissors for each game round.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
 //Computer picks randomly between rock, paper or scissors
+const compArray =["rock", "paper", "scissors"]
+
+function computerSelection() {
+    return compArray[Math.floor(Math.random() * compArray.length)];
+}
+
+let compWeapon = computerSelection()
+
+console.log(compWeapon)
 
 //User picks rock, paper, or scissors
 
