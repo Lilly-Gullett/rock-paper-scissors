5c05021099c009a0f5600577ed5bd9aa9407756d
Lilly Gullett <141647933+Lilly-Gullett@users.noreply.github.com>
2023-08-27 14:56:59 -0600

    Add user selection logic to javascript
    This commit adds logic to javascript.js to apply value to the user's
    selection via button pressed on index.html

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@
 const compArray =["rock", "paper", "scissors"]
 
 function computerSelection() {
-    return compArray[Math.floor(Math.random() * compArray.length)];
+    return compArray[Math.floor(Math.random() * compArray.length)]
 }
 
 let compWeapon = computerSelection()
@@ -10,6 +10,26 @@ let compWeapon = computerSelection()
 console.log(compWeapon)
 
 //User picks rock, paper, or scissors
+let userWeapon;
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+rock.addEventListener('click', () => {
+    userWeapon = 'rock';
+    console.log(userWeapon)
+} )
+
+paper.addEventListener('click', () => {
+    userWeapon = 'paper';
+    console.log(userWeapon)
+} )
+
+scissors.addEventListener('click', () => {
+    userWeapon = 'scissors';
+    console.log(userWeapon)
+} )
+
 
 //use if else statements to determine a winner
 
