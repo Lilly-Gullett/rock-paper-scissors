9170721ae207caed188db9de0abde6ecc80387bf
Lilly Gullett <141647933+Lilly-Gullett@users.noreply.github.com>
2023-08-27 13:50:44 -0600

    Add pseudo-code to javascript.js
    This commit adds pseudo-code to javascript to represent the different
    steps that will be coded to the program.

---
+++ javascript.js
@@ -0,0 +1,9 @@
+//Computer picks randomly between rock, paper or scissors
+
+//User picks rock, paper, or scissors
+
+//use if else statements to determine a winner
+
+//display a message in .win-loss-message to show winner
+
+//display win-loss count in .win-loss-count
