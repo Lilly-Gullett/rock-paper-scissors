50172bbd1d7ab366172a9344bfb0c424f26bbbbd
Lilly Gullett <141647933+Lilly-Gullett@users.noreply.github.com>
2023-08-28 15:49:32 -0600

    Add javascript logic to check winner
    This commit adds logic to javascript to check who won rock, paper,
    scissors. When any of the 3 buttons are clicked, the logic runs with if/
    else statements to check the computer's randomly selected choice against
    the user's selection.

--- javascript.js
+++ javascript.js
@@ -14,6 +14,7 @@ let userWeapon;
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
+const button = document.querySelectorAll('button');
 
 rock.addEventListener('click', () => {
     userWeapon = 'rock';
@@ -32,6 +33,46 @@ scissors.addEventListener('click', () => {
 
 
 //use if else statements to determine a winner
+let winner;
+
+for (let i= 0; i < button.length;i++) {
+    button[i].addEventListener("click", () => {
+        checkWinner();
+        console.log(winner)
+    })
+}
+
+
+function checkWinner() {
+    if (userWeapon==='rock'){
+        if (compWeapon==='rock') {
+            winner='tie'
+        } else if (compWeapon ==='scissors') {
+            winner = 'user'
+        } else if (compWeapon==='paper') {
+            winner = 'comp'
+        }
+    } else if (userWeapon ==='paper'){
+        if (compWeapon==='paper') {
+            winner='tie'
+        } else if (compWeapon==='rock') {
+            winner = 'user'
+        } else if (compWeapon === 'scissors') {
+            winner = 'comp'
+        }
+    } else if (userWeapon==='scissors'){
+        if (compWeapon==='scissors') {
+            winner='tie'
+        } else if (compWeapon==='paper'){
+            winner = 'user'
+        } else if (compWeapon === 'rock') {
+            winner = 'comp'
+        }
+    }
+}
+
+
+
 
 //display a message in .win-loss-message to show winner
 
